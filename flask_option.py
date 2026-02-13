930a4d49b29fd29466ce29bbe4c1bd283dcda911
nida-er <nida-er@github.com>
2026-02-13 16:22:51 +0300

    flask_env completed

--- flask_option.py
+++ flask_option.py
@@ -4,7 +4,16 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    # read the variable of FLASK_ENV
+    env = os.getenv("FLASK_ENV")
+    
+    if env == "development":
+        return "Starting in development mode..."
+    elif env == "production":
+        return "Starting in production mode..."
+    else:
+        # if variable is not set or has an unexpected value,
+        return "Starting in empty mode..."
 
 if __name__ == "__main__":
     print(start())
